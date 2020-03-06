e674c56ace17c5ab6f940756e36bceb3248528ed
paul-pow <60316488+paul-pow@users.noreply.github.com>
2020-03-06 13:14:09 +0100

    Update 2 Reader.ino

--- 2 Reader.ino
+++ 2 Reader.ino
@@ -7,8 +7,45 @@
 #define SS_PIN_2 48 
 #define RST_PIN_2 6 
 
+
 MFRC522 mfrc522(SS_PIN, RST_PIN); // RFID-Empfänger benennen
 MFRC522 mfrc522_2(SS_PIN_2, RST_PIN_2); // RFID-Empfänger benennen
+
+const int AnzahlChips = 3;      //deklariert die Anzahl der Chips, die initial vorhanden sind
+byte RFIDids[AnzahlChips]{}; 
+
+void WhitelistSchreiben(){
+    
+   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()){
+    
+      boolean cResult = false;
+      for (byte  i=0; i<AnzahlChips; i++){
+        if(RFIDids[i] == mfrc522.uid.uidByte){
+          cResult=true;
+          Serial.println("ID istbereits gespeichert!");
+          break;
+        }
+      }
+      if(!cResult){
+        int Speicherstelle = sizeof(RFIDids)/sizeof(RFIDids[0]) + 1;
+        RFIDids[Speicherstelle] = mfrc522.uid.uidByte;
+       Serial.print("Code ");
+       
+       
+        for (byte i = 0; i < mfrc522.uid.size; i++)
+      {
+      Serial.print(mfrc522.uid.uidByte[i]); 
+      Serial.print(","); 
+      }
+  
+       Serial.print(" an Stelle ");
+       Serial.print(Speicherstelle);
+       Serial.println("gespeichert");
+    } 
+    
+   }
+}
+
 
 void LesenAusgeben(){ //Diese Methode liest den Chip am ersten Sensor aus speichert seinen Code in ein Array und gibt ihn aus
   if ( ! mfrc522.PICC_IsNewCardPresent()) // Wenn keine Karte in Reichweite ist...
@@ -25,7 +62,30 @@ void LesenAusgeben(){ //Diese Methode liest den Chip am ersten Sensor aus speich
 
   for (byte i = 0; i < mfrc522.uid.size; i++)
     {
-    Serial.print(mfrc522.uid.uidByte[i], HEX); // Dann wird die UID ausgelesen, die aus vier einzelnen Blöcken besteht und der Reihe nach an den Serial Monitor gesendet. Die Endung Hex bedeutet, dass die vier Blöcke der UID als HEX-Zahl (also auch mit Buchstaben) ausgegeben wird
+    Serial.print(mfrc522.uid.uidByte[i]); // Dann wird die UID ausgelesen, die aus vier einzelnen Blöcken besteht und der Reihe nach an den Serial Monitor gesendet. Die Endung Hex bedeutet, dass die vier Blöcke der UID als HEX-Zahl (also auch mit Buchstaben) ausgegeben wird
+    Serial.print(" "); // Der Befehl „Serial.print(" ");“ sorgt dafür, dass zwischen den einzelnen ausgelesenen Blöcken ein Leerzeichen steht.
+    }
+
+  Serial.println(); // Mit dieser Zeile wird auf dem Serial Monitor nur ein Zeilenumbruch gemacht.
+  }
+
+void LesenAusgebenS2 (){
+   
+    if ( ! mfrc522_2.PICC_IsNewCardPresent()) // Wenn keine Karte in Reichweite ist...
+    {
+    return; // ...springt das Programm zurück vor die if-Schleife, womit sich die Abfrage wiederholt.
+    }
+
+  if ( ! mfrc522_2.PICC_ReadCardSerial()) // Wenn kein RFID-Sender ausgewählt wurde
+    {
+    return; // ...springt das Programm zurück vor die if-Schleife, womit sich die Abfrage wiederholt.
+    }
+
+  Serial.print("Die ID des RFID-TAGS am zweiten Sensor lautet:"); // "Die ID des RFID-TAGS lautet:" wird auf den Serial Monitor geschrieben.
+
+  for (byte i = 0; i < mfrc522_2.uid.size; i++)
+    {
+    Serial.print(mfrc522_2.uid.uidByte[i]); // Dann wird die UID ausgelesen, die aus vier einzelnen Blöcken besteht und der Reihe nach an den Serial Monitor gesendet. Die Endung Hex bedeutet, dass die vier Blöcke der UID als HEX-Zahl (also auch mit Buchstaben) ausgegeben wird
     Serial.print(" "); // Der Befehl „Serial.print(" ");“ sorgt dafür, dass zwischen den einzelnen ausgelesenen Blöcken ein Leerzeichen steht.
     }
 
@@ -33,16 +93,28 @@ void LesenAusgeben(){ //Diese Methode liest den Chip am ersten Sensor aus speich
   }
 
 
+
 void setup(){ // Beginn des Setups:
+
   Serial.begin(9600); // Serielle Verbindung starten (Monitor)
+
   SPI.begin(); // SPI-Verbindung aufbauen
 
   mfrc522.PCD_Init(); // Initialisierung des RFID-Empfängers zum Lesen und Ausgeben
   mfrc522_2.PCD_Init(); // Initialisierung des RFID-Empfängers zum schreiben der Whitelist
+
 }
 
 
 
+
+
+
+
 void loop() {// Hier beginnt der Loop-Teil
+ 
 LesenAusgeben();
+LesenAusgebenS2 ();
+WhitelistSchreiben();
+
 }
